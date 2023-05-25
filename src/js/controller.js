d6487db4f9663c57f187dcf41db2f5d8aa097268
Svetlana Radisic <svetlana_radisic@yahoo.com>
2023-05-25 19:40:59 +0200

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,7 +124,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
   // location.reload();
 };
-
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -133,6 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
